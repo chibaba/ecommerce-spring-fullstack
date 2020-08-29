9f814e7d0b26d3bc63a6b8f7acef34e3b449fe0b
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-29 02:35:19 +0100

    wip

--- src/main/java/com/nedstack/springbootecommerce/entity/Product.java
+++ src/main/java/com/nedstack/springbootecommerce/entity/Product.java
@@ -3,16 +3,21 @@ package com.nedstack.springbootecommerce.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name="product")
 @Data
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name="sku")
     private String sku;
 
     private String name;
@@ -24,4 +29,10 @@ public class Product {
     private String imageUrl;
 
     private boolean active;
+
+    private int  unitsInStock;
+
+    private Date dateCreated;
+
+    private Date lastCreated;
 }
