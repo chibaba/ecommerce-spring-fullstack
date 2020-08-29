74e30f72ecabec83fe5bb52dc14668bc3227f0db
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-29 02:21:52 +0100

    instatiating variable for lombok

--- src/main/java/com/nedstack/springbootecommerce/entity/Product.java
+++ src/main/java/com/nedstack/springbootecommerce/entity/Product.java
@@ -1,11 +1,27 @@
 package com.nedstack.springbootecommerce.entity;
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name="product")
-
+@Data
 public class Product {
+    private Long id;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private boolean active;
 }
