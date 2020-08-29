46b87a04b7780f37f5d8c25c1d82bec140b86c82
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-29 02:46:06 +0100

    creating timesta,p for hibernate to manage

--- src/main/java/com/nedstack/springbootecommerce/entity/Product.java
+++ src/main/java/com/nedstack/springbootecommerce/entity/Product.java
@@ -2,6 +2,8 @@ package com.nedstack.springbootecommerce.entity;
 
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -20,19 +22,29 @@ public class Product {
     @Column(name="sku")
     private String sku;
 
+    @Column(name="name")
     private String name;
 
+    @Column(name="description")
     private String description;
 
+    @Column(name="unit_Price")
     private BigDecimal unitPrice;
 
+    @Column(name="image_url")
     private String imageUrl;
 
+    @Column(name="active")
     private boolean active;
 
+    @Column(name="units_in_Stock")
     private int  unitsInStock;
 
+    @Column(name="date_created")
+    @CreationTimestamp
     private Date dateCreated;
 
+    @Column(name="last_created")
+    @UpdateTimestamp
     private Date lastCreated;
 }
